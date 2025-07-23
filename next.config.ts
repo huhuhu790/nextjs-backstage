edb59b476bcee03932455788d9bbd92ac5d43032
huhuhu790 <vetur@foxmail.com>
2025-07-23 13:11:21 +0800

    更新nextjs包导出

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    serverExternalPackages: ["@confluentinc/kafka-javascript"],
     experimental: {
         nodeMiddleware: true,
     }
